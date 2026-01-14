ded478fb6ead3ad0f614aed652601f55850bb9ed
marclura <29801332+marclura@users.noreply.github.com>
2026-01-14 12:21:55 +0100

    Update 07-RGB-LCD.ino

--- 07-RGB-LCD/07-RGB-LCD.ino
+++ 07-RGB-LCD/07-RGB-LCD.ino
@@ -12,6 +12,11 @@ int blue = 0;
 // sensors
 int pot = 0;
 
+// timing
+unsigned long old_millis_counter = 0;
+unsigned long old_millis_pot = 0;
+const int counter_interval = 1000;  // 1000ms, 1s
+
 
 void setup() {
   lcd.begin(16, 2);
@@ -22,24 +27,35 @@ void setup() {
 void loop() {
 
   // potentiometer
-  pot = analogRead(A1);
-  lcd.setCursor(12, 1);   // col (12), line (1)
-  lcd.print("    ");
-  lcd.setCursor(12, 1);
-  lcd.print(pot);
+  if(millis() - old_millis_pot > 100) { // update pot every 100ms
+    
+    // potentiomenter smooting
+    pot = 0;
+    for(int i=0; i<20; i++) {
+      pot += analogRead(A1);
+    }
+    pot /= 20;  // pot = pot / 20;
 
-  // counter
-  lcd.setCursor(0, 1);  // first col (0), second line (1)
-  lcd.print(counter);
-  counter++;
+    lcd.setCursor(12, 1);   // col (12), line (1)
+    lcd.print("    ");
+    lcd.setCursor(12, 1);
+    lcd.print(pot);
+    old_millis_pot = millis();
+  }
 
-  // RGB backlight
-  red = randomColor(); // 0 .. 255, 2^8 (1byte)
-  green = randomColor(); // 0 .. 255, 2^8 (1byte)
-  blue = randomColor(); // 0 .. 255, 2^8 (1byte)
-  lcd.setRGB(red, green, blue);
+  // seconds counter
+  if(millis() - old_millis_counter > counter_interval) {
+    lcd.setCursor(0, 1);  // first col (0), second line (1)
+    lcd.print(counter);
+    counter++;
+    old_millis_counter = millis();
 
-  delay(1000);
+    // RGB backlight
+    red = randomColor(); // 0 .. 255, 2^8 (1byte)
+    green = randomColor(); // 0 .. 255, 2^8 (1byte)
+    blue = randomColor(); // 0 .. 255, 2^8 (1byte)
+    lcd.setRGB(red, green, blue);
+  }
 }
 
 int randomColor() {
