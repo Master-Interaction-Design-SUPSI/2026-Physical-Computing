d6e3910f9e418cae57a819052dca58c726ff1ab8
marclura <29801332+marclura@users.noreply.github.com>
2026-01-15 15:31:33 +0100

    Update neopixels.ino

--- 10-webSerial-demo/10-webSerial-demo/neopixels.ino
+++ 10-webSerial-demo/10-webSerial-demo/neopixels.ino
@@ -1,18 +1,20 @@
 void updatePixels(String msg) {
-  int r, g, b;
-  parseRGB(msg, r, g, b);
-  Serial.println(String(r) + "," + String(g) + "," + String(b));
+  int r, g, b, i;
+  parseRGB(msg, r, g, b, i);
+  Serial.println(String(i) + "," + String(r) + "," + String(g) + "," + String(b));
 
-  ring.setPixelColor(0, ring.Color(r, g, b));
+  ring.setPixelColor(i, ring.Color(r, g, b));
   ring.show();
 }
 
-void parseRGB(String data, int &r, int &g, int &b) {
+void parseRGB(String data, int &r, int &g, int &b, int &i) {
   int firstComma  = data.indexOf(',');
   int secondComma = data.indexOf(',', firstComma + 1);
   int thirdComma  = data.indexOf(',', secondComma + 1);
+  int fourthComma  = data.indexOf(',', thirdComma + 1);
 
   r = data.substring(firstComma + 1, secondComma).toInt();
   g = data.substring(secondComma + 1, thirdComma).toInt();
   b = data.substring(thirdComma + 1).toInt();
+  i = data.substring(fourthComma + 1).toInt();
 }
